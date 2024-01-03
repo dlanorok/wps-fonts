58bfe0968f8efdeaf6a43a3c711ac3682e73c990
Ronald Ramirez <dlanorok@hotmail.com>
2024-01-03 18:35:21 -0500

    Install missing fonts for wps

---
+++ install-fonts.sh
@@ -0,0 +1,5 @@
+sudo mkdir /usr/share/fonts/kingsoft
+sudo cp ./wps /usr/share/fonts/wps-office/
+sudo chown -R $USER:$USER /usr/share/fonts/kingsoft
+sudo chmod -R o+rw,g+rw /usr/share/fonts/kingsoft
+sudo fc-cache -vfs
