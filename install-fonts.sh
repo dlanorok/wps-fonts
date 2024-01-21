f41875d2b980fa971d872fc866954f4079fa5d21
Ronald Ramirez Moran <dlanorok@hotmail.com>
2024-01-21 03:43:47 -0500

    update folder name

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,5 +1,5 @@
 sudo mkdir /usr/share/fonts/kingsoft
-sudo cp -r ./wps /usr/share/fonts/wps-office/
+sudo cp -r ./wps /usr/share/fonts/kingsoft
 sudo chown -R $USER:$USER /usr/share/fonts/kingsoft
 sudo chmod -R o+rw,g+rw /usr/share/fonts/kingsoft
 sudo fc-cache -vfs
